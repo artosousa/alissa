6fa211142a87dfe4047d4090dece6d022293a9d2
Arthur Sousa <macbookpro16@Arthurs-MacBook-Pro.local>
2022-01-16 11:25:29 -0500

    removed attempt at pc fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -165,12 +165,7 @@ const styles = [
   'theTalley',
   'fromTheDeskOf'
 ]
-let OSName="Unknown OS";
-if (navigator.appVersion.indexOf("Win")!=-1) OSName="Windows";
 
-if(OSName === 'Windows'){
-  document.body.classList.add("Windows");
-}
 // markup
 const IndexPage = ({data}) => {
   const [style, setStyle] = useState('theWisdom');
