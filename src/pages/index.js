9c05f85dc782f2d4ef056208b6f4d14fe86d7dc4
Arthur Sousa <macbookpro16@Arthurs-MacBook-Pro.local>
2022-04-09 09:21:11 -0400

    text changes as per email

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { graphql } from 'gatsby';
 
-import "../css/type.css";
+import '../css/type.css';
 
 import {
   Box,
@@ -27,7 +27,7 @@ import {
   Stack,
   Text,
   useDisclosure,
-  Textarea
+  Textarea,
 } from '@chakra-ui/react';
 
 export const pageQuery = graphql`
@@ -116,7 +116,7 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
 const colors = [
   ['Powder', '#ADC1DC', '#FFF'],
   ['Denim', '#2C5B8E', '#FFF'],
@@ -147,13 +147,13 @@ const envelopeColors = [
   ['Paprika', '#dc6f52', '#fff'],
   ['Nude', '#f6e6d6', '#fffbd3'],
   ['Lilac', '#cfbcd0', '#fff'],
-  ['Emerald','#37443d', '#fff'],
+  ['Emerald', '#37443d', '#fff'],
   ['Disco', '#d3d7d8', '#fff'],
   ['Charcoal', '#2d2b2e', '#fff'],
   ['Calla Lily', '#e7e7e7', '#fff'],
   ['California Poppy', '#d78023', '#fff'],
   ['Barragan Pink', '#d53863', '#fff'],
-]
+];
 
 const styles = [
   'theHenry',
@@ -161,18 +161,18 @@ const styles = [
   'thePalermo',
   'theDylan',
   'theTalley',
-  'fromTheDeskOf'
-]
+  'fromTheDeskOf',
+];
 
 // markup
-const IndexPage = ({data}) => {
+const IndexPage = ({ data }) => {
   const [style, setStyle] = useState('theJordan');
   const [colorIndex, setColorIndex] = useState(0);
   const color = colors[colorIndex];
   const [envelopColorIndex, setEnvelopeColorIndex] = useState(0);
   const envelopeColor = envelopeColors[envelopColorIndex];
-  const [fontId, setFontId]  = useState('c30b0034-34db-56e7-8012-213832df2366');
-  const font = data[style]?.nodes.find(node => node.id === fontId);
+  const [fontId, setFontId] = useState('c30b0034-34db-56e7-8012-213832df2366');
+  const font = data[style]?.nodes.find((node) => node.id === fontId);
   const [cardCopy, setCardCopy] = useState('Jordan');
   const [jordanCopy, setJordanCopy] = useState('AUBREE');
   const [envCopy, setEnvCopy] = useState('616 YESTERDAY AVENUE | BEACON, NY 12508');
@@ -180,33 +180,31 @@ const IndexPage = ({data}) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const btnRef = React.useRef();
   return (
-    
-    <Box  bg='#e9e9e9' height='100vh' display='inline-flex' w='100%' alignItems='center' justifyContent='center'>
-      <Flex  paddingTop="50"  alignItems='center' justifyContent='center' flexWrap='wrap' overflowX='hidden' flexDirection='column' w="100%" maxWidth='583px'>
+
+    <Box bg="#e9e9e9" height="100vh" display="inline-flex" w="100%" alignItems="center" justifyContent="center">
+      <Flex paddingTop="50" alignItems="center" justifyContent="center" flexWrap="wrap" overflowX="hidden" flexDirection="column" w="100%" maxWidth="583px">
         <title>Alissa Bell</title>
-        <Button fontFamily='Montserrat' marginBottom='16px' w={['80%', '100%', '100%', '100%']} borderRadius='0' ref={btnRef} backgroundColor='#C67527' color='#ffffff' _hover={{background:'#a8561c'}} onClick={onOpen}>
+        <Button fontFamily="Montserrat" fontSize="11" fontWeight="bolder" marginBottom="16px" w={['80%', '100%', '100%', '100%']} borderRadius="0" ref={btnRef} backgroundColor="#C67527" color="#ffffff" _hover={{ background: '#a8561c' }} onClick={onOpen}>
           Customize Your Stationery
         </Button>
-        
-        
-        <Flex className={`cardParent ${style} `} w={['100%','auto', 'auto', 'auto']} alignItems='center' justifyContent='center' flexWrap='wrap'>
-          <Flex className={`envelopeBG`}   bgColor={envelopeColor[1]}  >
-            <Flex className={`envelopeMask`}    color='#fff'  backgroundSize='100%' w={['410px','500px','583px','583px']} h={['310px','377px','435px','435px']} position='relative'  justifyContent='center' >
-              <Text as='h1' fontFamily='Proxima' letterSpacing='3.125px' whiteSpace='pre-wrap' textAlign='center' fontSize='12' fontFamily={font?.type} letterSpacing={`${font?.kerning/16}px`} color={color[1]} textShadow='0px 1px 1px rgb(255 255 255 / 54%), 0px -0.5px 0px rgb(0 0 0 / 38%)'  >{envCopy}</Text>
+
+        <Flex className={`cardParent ${style} `} w={['100%', 'auto', 'auto', 'auto']} alignItems="center" justifyContent="center" flexWrap="wrap">
+          <Flex className="envelopeBG" bgColor={envelopeColor[1]}>
+            <Flex className="envelopeMask" color="#fff" backgroundSize="100%" w={['410px', '500px', '583px', '583px']} h={['310px', '377px', '435px', '435px']} position="relative" justifyContent="center">
+              <Text as="h1" fontFamily="Proxima" letterSpacing="3.125px" whiteSpace="pre-wrap" textAlign="center" fontSize="12" fontFamily={font?.type} letterSpacing={`${font?.kerning / 16}px`} color={color[1]} textShadow="0px 1px 1px rgb(255 255 255 / 54%), 0px -0.5px 0px rgb(0 0 0 / 38%)">{envCopy}</Text>
             </Flex>
           </Flex>
-          { console.log(font)}
-          <Flex className={`card `} flexWrap='wrap'  w={['500px','500px','583px','583px']} h={['327px','377px','435px','435px']} position='relative' left='5px' bgImage={`url(${style}.jpg)`} backgroundSize='100%' p={['10','10','16','16']}>
-            { style === 'fromTheDeskOf' &&
+          <Flex className="card " flexWrap="wrap" w={['500px', '500px', '583px', '583px']} h={['327px', '377px', '435px', '435px']} position="relative" left="5px" bgImage={`url(${style}.jpg)`} backgroundSize="100%" p={['10', '10', '16', '16']}>
+            { style === 'fromTheDeskOf'
+              && (
               <>
-                <Text as='h2' color={color[1]} fontFamily={font?.topType} className={font?.topType} fontSize='14px'>From the Desk Of</Text>
+                <Text as="h2" color={color[1]} fontFamily={font?.topType} className={font?.topType} fontSize="14px">From the Desk Of</Text>
               </>
-            }
-            <Text as={style === 'theJordan' ? 'h2' : 'h1'} fontSize='16' textShadow='0px 1px 1px rgb(255 255 255 / 94%), 0px -0.5px 0px rgb(0 0 0 / 38%)' className={style}  fontFamily={style === 'theJordan' ? font?.topType : font?.type}  letterSpacing={`${font?.kerning/16}px`} color={color[1]}>{cardCopy}</Text>
-            { style === 'theJordan' &&
-              <Text as='h1' borderBottom='1px solid' color={color[1]} fontFamily={font?.type}>{jordanCopy}</Text>
-            }
-            
+              )}
+            <Text as={style === 'theJordan' ? 'h2' : 'h1'} fontSize="16" textShadow="0px 1px 1px rgb(255 255 255 / 94%), 0px -0.5px 0px rgb(0 0 0 / 38%)" className={style} fontFamily={style === 'theJordan' ? font?.topType : font?.type} letterSpacing={`${font?.kerning / 16}px`} color={color[1]}>{cardCopy}</Text>
+            { style === 'theJordan'
+              && <Text as="h1" borderBottom="1px solid" color={color[1]} fontFamily={font?.type}>{jordanCopy}</Text>}
+
           </Flex>
         </Flex>
         <Drawer
@@ -214,24 +212,31 @@ const IndexPage = ({data}) => {
           placement="left"
           onClose={onClose}
           size="sm"
-          scrollBehavior={'inside'} blockScrollOnMount={false}
+          scrollBehavior="inside"
+          blockScrollOnMount={false}
         >
-          
-          <DrawerContent fontFamily='Montserrat' overflow='auto'>
-            <DrawerCloseButton />
-            <DrawerHeader fontFamily='Cormorant Garamond'  fontSize='1.6rem'>Envelope Settings</DrawerHeader>
-            <Container >
-              <Text  >Envelope Text:</Text>
-              <Textarea spacing='4' whiteSpace='pre-wrap' value={envCopy} placeholder="Envelope Text" size="lg" onChange={(event) => setEnvCopy(event.target.value)} />
 
-              { !styles.includes(style) && 
+          <DrawerContent fontFamily="Montserrat" overflow="auto">
+
+            <DrawerCloseButton />
+
+            <DrawerHeader fontFamily="Cormorant Garamond" fontSize="1.6rem">
+              <Text fontFamily="Montserrat" fontSize="1rem" fontWeight="lighter" marginBottom="15px">Use our editor to select your design, font, color and message.  After adding to cart &amp; purchasing, we will email a digital proof with your selections.</Text>
+              Envelope Selections
+            </DrawerHeader>
+            <Container>
+              <Text>Envelope Text:</Text>
+              <Textarea spacing="4" whiteSpace="pre-wrap" value={envCopy} placeholder="Envelope Text" size="lg" onChange={(event) => setEnvCopy(event.target.value)} />
+
+              { !styles.includes(style)
+                && (
                 <>
-                  <Text marginTop='16px' spacing='4'>Select Envelope Color:</Text>
+                  <Text marginTop="16px" spacing="4">Select Envelope Color:</Text>
                   <Popover
                     isLazy
                     variant="picker"
                     placement="top-end"
-                    spacing='4'
+                    spacing="4"
                   >
                     <PopoverTrigger>
                       <Button
@@ -283,16 +288,15 @@ const IndexPage = ({data}) => {
                     </PopoverContent>
                   </Popover>
                 </>
-              }
+                )}
             </Container>
-            <DrawerHeader fontFamily='Cormorant Garamond' fontSize='1.6rem'>Card Settings</DrawerHeader>
+            <DrawerHeader fontFamily="Cormorant Garamond" fontSize="1.6rem">Card Selections</DrawerHeader>
             <Container>
-              
 
-              <Stack spacing='4'>
+              <Stack spacing="4">
                 <Text a>Card Style:</Text>
-                <Select value={style} onChange={(event) => setStyle(event.target.value)} placeholder="Select Style" >
-                <option value="theJordan">The Jordan</option>
+                <Select value={style} onChange={(event) => setStyle(event.target.value)} placeholder="Select Style">
+                  <option value="theJordan">The Jordan</option>
                   <option value="theWisdom">The Wisdom</option>
                   <option value="theBri">The Bri</option>
                   <option value="theAudra">The Audra</option>
@@ -304,31 +308,31 @@ const IndexPage = ({data}) => {
                   <option value="fromTheDeskOf">From The Desk</option>
                 </Select>
                 <Text>Font Family:</Text>
-                { style &&
-                
-                  <Select spacing='4' value={fontId} placeholder="Select Font Family" onChange={(event) => setFontId(event.target.value)} > 
-                    {data[style].nodes.map(node => {
-                      return (
-                        <option key={node.id} value={node.id}>
-                          {/* {node.topType ? `${node.topType} & ${node.type} ` : node.type} */}
-                          {node.topLabel && `${node.topLabel}` }
-                          {node.kerning && ` - ${node.kerning}`}
-                        </option>
-                      )
-                    })}
+                { style
+
+                  && (
+                  <Select spacing="4" value={fontId} placeholder="Select Font Family" onChange={(event) => setFontId(event.target.value)}>
+                    {data[style].nodes.map((node) => (
+                      <option key={node.id} value={node.id}>
+                        {/* {node.topType ? `${node.topType} & ${node.type} ` : node.type} */}
+                        {node.topLabel && `${node.topLabel}` }
+                        {node.kerning && ` - ${node.kerning}`}
+                      </option>
+                    ))}
                   </Select>
-                }
+                  )}
               </Stack>
-              <Text marginTop='16px'>{style === 'theJordan' ? 'Top Text:' : 'Card Text'}</Text>
-              <Input spacing='4' value={cardCopy} placeholder="Top Text" size="lg" onChange={(event) => setCardCopy(event.target.value)} />
-              
-              {style === 'theJordan' && 
+              <Text marginTop="16px">{style === 'theJordan' ? 'Top Text:' : 'Card Text'}</Text>
+              <Input spacing="4" value={cardCopy} placeholder="Top Text" size="lg" onChange={(event) => setCardCopy(event.target.value)} />
+
+              {style === 'theJordan'
+                && (
                 <>
-                  <Text as="p" marginTop='16px'>Bottom Text:</Text>
-                  <Input spacing='4' value={jordanCopy} placeholder="Top Text" size="lg" onChange={(event) => setJordanCopy(event.target.value)} />
+                  <Text as="p" marginTop="16px">Bottom Text:</Text>
+                  <Input spacing="4" value={jordanCopy} placeholder="Top Text" size="lg" onChange={(event) => setJordanCopy(event.target.value)} />
                 </>
-              }
-              <Text marginTop='16px'>Select Ink Color:</Text>
+                )}
+              <Text marginTop="16px">Select Ink Color:</Text>
               <Popover
                 isLazy
                 variant="picker"
@@ -386,7 +390,7 @@ const IndexPage = ({data}) => {
             <DrawerBody />
           </DrawerContent>
         </Drawer>
-    </Flex>
+      </Flex>
     </Box>
   );
 };
